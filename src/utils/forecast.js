c46ac35e62d44a42b5c788e261a7eda172349d8d
Anchal Verma <vanchal964@gmail.com>
2022-02-08 20:20:13 +0530

    Error handling inclusion

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,10 +6,10 @@ function forecast(lat,long,callback){
     url = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+long+"&APPID=422afefbebd4f36a8304168a08d51184&units=us"
     request({url:url,json:true},(error,response)=>{
         if(error){
-            callback("Weather Service unavailable",undefined)
+            callback({error:"Weather Service unavailable"},undefined)
         }
         else if(response.body.message){
-            callback("Unable to fetch data.try again",undefined)
+            callback({error:"Unable to fetch data.try again"},undefined)
         }
         else{
             callback(undefined," Weather: "+response.body.list[0].weather[0].description+". It is currently "+response.body.list[0].main.temp+" degrees. There is "+response.body.list[0].main.humidity+" humidity.")
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -8,10 +8,10 @@ function geocode(address,callback){
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURIComponent(address)+".json?access_token=pk.eyJ1IjoiYW5jaGFsLXYiLCJhIjoiY2t5cmZiYXppMHQxaDJubzF3Y2c4YnVoZyJ9.PASZ0ggudMQ85_ehCSYikw"
     request({url:url,json:true},(error,response)=>{
         if(error){
-            callback("Unable to connect to location",undefined)
+            callback({error:"Unable to connect to location"},undefined)
         }
         else if(response.body.features.length===0){
-            callback("Unable to find.Try again",undefined)
+            callback({error:"Unable to find.Try again"},undefined)
         }
         else{
             callback(undefined,{
